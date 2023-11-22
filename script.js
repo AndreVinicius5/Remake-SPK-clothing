cf7b026ab78c401bfecca8e3f5a98f5e5486480c
AndreVinicius5 <contato.andrevini5@gmail.com>
2023-11-22 11:27:47 -0300

    Excesso removido

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ if (window.location.pathname.endsWith("index.html")) {
   }
 }
 
-// Corrige os espaços e acentos
+
 function displayFileName(fileNameId) {
     const fileNameElement = document.getElementById(fileNameId);
     if (fileNameElement) {
@@ -29,7 +29,7 @@ function displayFileName(fileNameId) {
 }
 }
 
-// Chamar a função para exibir o nome do arquivo para cada item
+
 displayFileName('fileName1');
 displayFileName('fileName2');
 displayFileName('fileName3');
@@ -46,14 +46,14 @@ displayFileName('fileName14');
 displayFileName('fileName15');
 
  document.addEventListener("DOMContentLoaded", function() {
-//troca o nome no topo da pagina
+
 function setHeaderTitle() {
-  // Obtém o elemento h1 com o id destaques1
+ 
   const h1Element = document.querySelector('#destaques1 h1');
 
-  // Verifica se o elemento h1 foi encontrado
+  
   if (h1Element) {
-    // Define o conteúdo do h1 para "moletom"
+  
     h1Element.textContent = "Moletons";
   }
 }
@@ -61,9 +61,9 @@ function setHeaderTitle() {
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  // Verifica se a página atual é "moletom.html"
+
   if (window.location.pathname.endsWith("moletom.html")) {
-      // Obtém a referência para a imagem pelo ID
+    
       var imagemProduto = document.getElementById("imagemProduto1");
       var imagemProduto = document.getElementById("imagemProduto2");
       var imagemProduto = document.getElementById("imagemProduto3");
@@ -80,9 +80,9 @@ document.addEventListener("DOMContentLoaded", function() {
       var imagemProduto = document.getElementById("imagemProduto14");
       var imagemProduto = document.getElementById("imagemProduto15");
 
-      // Verifica se o elemento foi encontrado antes de tentar alterar o atributo src
+      
       if (imagemProduto) {
-          // Altera o caminho da imagem
+         
           imagemProduto1.src =  "moletom/Moletom SPK CHAMBER.png";
           imagemProduto2.src = "moletom/Moletom SPK N1 BLACK.png";
           imagemProduto3.src = "moletom/Moletom SPK N1 WHITE.png";
@@ -99,17 +99,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.addEventListener("DOMContentLoaded", function() {
   if (window.location.pathname.endsWith("moletom.html")) {
-    // Itera sobre os elementos com IDs de item8 até item15
+
     for (var i = 8; i <= 15; i++) {
-        // Constrói o ID dinamicamente
+        
         var idItem = "item" + i;
 
-        // Obtém a referência para o elemento pelo ID
+       
         var elemento = document.getElementById(idItem);
 
-        // Verifica se o elemento foi encontrado antes de tentar alterar o estilo
+        
         if (elemento) {
-            // Oculta o elemento
+            
             elemento.style.display = "none";
         } else {
             console.error("Elemento com ID '" + idItem + "' não encontrado na página.");
@@ -136,7 +136,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  // Array de caminhos para as imagens
+  
   var caminhosDasImagens = [
       "moletom/Moletom SPK CHAMBER.png",
       "moletom/Moletom SPK N1 BLACK.png",
@@ -145,28 +145,28 @@ document.addEventListener("DOMContentLoaded", function() {
       "moletom/Moletom SPK  New POP.png"
   ];
 
-  // Itera sobre os elementos com classe "itemX"
+  
   for (var i = 1; i <= 15; i++) {
-      // Constrói o ID do item dinamicamente
+     
       var idItem = "item" + i;
 
-      // Obtém a referência para o elemento pelo ID
+      
       var elemento = document.getElementById(idItem);
 
-      // Verifica se o elemento foi encontrado antes de tentar alterar o conteúdo
+      
       if (elemento) {
-          // Obtém o caminho correspondente ao índice i
+       
           var caminhoDaImagem = caminhosDasImagens[i - 1];
 
-          // Extrai o nome do arquivo do caminho
+       
           var nomeDoArquivo = caminhoDaImagem.split('/').pop().split('.')[0];
 
-          // Obtém a referência para o elemento h3 dentro do elemento #itemX
+         
           var h3Element = elemento.querySelector('h3');
 
-          // Verifica se o elemento h3 foi encontrado antes de tentar alterar o conteúdo
+         
           if (h3Element) {
-              // Define o conteúdo do h3 como o nome do arquivo
+             
               h3Element.textContent = nomeDoArquivo;
           } else {
               console.error("Elemento h3 não encontrado dentro de '" + idItem + "'.");
